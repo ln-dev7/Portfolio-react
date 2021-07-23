574449909828df717ce6955a0fa981372f759b58
ln-dev7 <leonelngoya@gmail.com>
2021-07-23 02:02:43 +0100

    page d initialisation

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div className="logo">
+            <img src="./SchoolManager.png" alt="" />
+            <h1>BIENVENUS SUR SCHOOL MANAGER !</h1>
+        </div>
+    );
+};
+
+export default Logo;
